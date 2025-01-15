5228bee7bfae45435964e8a8d199d738e04b379b
SitharaPramodini <it21306754@my.sliit.lk>
2025-01-15 11:01:15 +0530

    fit path

--- src/App.jsx
+++ src/App.jsx
@@ -37,18 +37,18 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/stories" element={<StoriesHome />} />
-          <Route path="/service/:index" element={<Services />} />
+          <Route path="/fit/" element={<Home />} />
+          <Route path="/fit/aboutus" element={<AboutUs />} />
+          <Route path="/fit/stories" element={<StoriesHome />} />
+          <Route path="/fit/service/:index" element={<Services />} />
 
-          <Route path="/slider" element={<DatacenterDivider />} />
-          {/* <Route path="/consultancyservices" element={<ConsultancyServices />} /> */}
+          <Route path="/fit/slider" element={<DatacenterDivider />} />
+          {/* <Route path="/fit/consultancyservices" element={<ConsultancyServices />} /> */}
 
           {/* services */}
-          {/* <Route path="/stories" element={<StoriesHome />} /> */}
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/awards" element={<Stage />}></Route>
+          {/* <Route path="/fit/stories" element={<StoriesHome />} /> */}
+          <Route path="/fit/contact" element={<Contact />} />
+          <Route path="/fit/awards" element={<Stage />}></Route>
 
         </Routes>
       </Suspense>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
       >
         <div className="flex flex-col px-3 py-2.5 lg:px-5 lg:pl-3">
           <div className="flex items-center justify-between">
-            <Link to='/'>
+            <Link to='/fit/'>
               <img src="/fit/public/logo.webp" className="h-12 md:ml-12 my-2" alt="Logo" />
 
             </Link>
@@ -41,14 +41,14 @@ const Navbar = () => {
             {/* Desktop Navigation */}
             <div className="hidden md:flex flex-col">
               <ul className="flex flex-row items-center gap-5">
-                <Link to='/'>
+                <Link to='/fit/'>
                   <li className="relative group text-white font-medium text-lg px-4 pb-2 cursor-pointer">
                     Home
                     <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-500 transition-all duration-300 group-hover:w-full"></span>
                   </li>
                 </Link>
                 <span className="text-gray-400 pb-2">|</span>
-                <Link to='/aboutus'>
+                <Link to='/fit/aboutus'>
                   <li className="relative group text-white font-medium text-lg px-4 pb-2 cursor-pointer">
                     About us
                     <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-500 transition-all duration-300 group-hover:w-full"></span>
@@ -66,47 +66,47 @@ const Navbar = () => {
                   {isServicesDropdownOpen && (
                     <div className="absolute top-8 left-0 bg-gray-800 text-white rounded-md shadow-lg w-72 text-sm">
                       <ul className="flex flex-col p-4 space-y-1">
-                        <Link to='/service/0' onClick={handleLinkClick}>
+                        <Link to='/fit/service/0' onClick={handleLinkClick}>
                           <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                             Information Security
                           </li>
                         </Link>
-                        <Link to='/service/1' onClick={handleLinkClick}>
+                        <Link to='/fit/service/1' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Consultancy Services
                         </li>
                         </Link>
-                        <Link to='/service/2' onClick={handleLinkClick}>
+                        <Link to='/fit/service/2' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Cyber Security
                         </li>
                         </Link>
-                        <Link to='/service/3' onClick={handleLinkClick}>
+                        <Link to='/fit/service/3' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Data Center Solutions
                         </li>
                         </Link>
-                        <Link to='/service/4' onClick={handleLinkClick}>
+                        <Link to='/fit/service/4' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Collaboration Suites
                         </li>
                         </Link>
-                        <Link to='/service/5' onClick={handleLinkClick}>
+                        <Link to='/fit/service/5' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Next Generation Managed Service
                         </li>
                         </Link>
-                        <Link to='/service/6' onClick={handleLinkClick}>
+                        <Link to='/fit/service/6' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Unified Communication & PABX
                         </li>
                         </Link>
-                        <Link to='/service/7' onClick={handleLinkClick}>
+                        <Link to='/fit/service/7' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Enterprise Networking
                         </li>
                         </Link>
-                        <Link to='/service/8' onClick={handleLinkClick}>
+                        <Link to='/fit/service/8' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Innovation Center
                         </li>
@@ -116,7 +116,7 @@ const Navbar = () => {
                   )}
                 </li>
                 <span className="text-gray-400 pb-2">|</span>
-                <Link to='/awards'>
+                <Link to='/fit/awards'>
                   <li className="relative group text-white font-medium text-lg px-4 pb-2 cursor-pointer">
                     Awards
                     <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-orange-500 transition-all duration-300 group-hover:w-full"></span>
@@ -129,7 +129,7 @@ const Navbar = () => {
             <div className="hidden md:flex flex-col items-end">
               <Link
                 className="btn h-8 pt-1 text-white bg-orange-500 px-6 py-2 text-center rounded-lg hover:bg-orange-600 transition duration-300"
-                to="/contact"
+                to="/fit/contact"
               >
                 CONTACT US
               </Link>
@@ -161,7 +161,7 @@ const Navbar = () => {
                     <li>
                       <Link
                         className="block px-4 py-2 hover:bg-gray-700 rounded-lg"
-                        to="/"
+                        to="/fit/"
                       >
                         Home
                       </Link>
@@ -169,7 +169,7 @@ const Navbar = () => {
                     <li>
                       <Link
                         className="block px-4 py-2 hover:bg-gray-700 rounded-lg"
-                        to="/aboutus"
+                        to="/fit/aboutus"
                       >
                         About Us
                       </Link>
@@ -183,47 +183,47 @@ const Navbar = () => {
                       </button>
                       {isServicesDropdownOpen && (
                         <ul className="flex flex-col space-y-2 mt-2">
-                        <Link to='/service/0' onClick={handleLinkClick}>
+                        <Link to='/fit/service/0' onClick={handleLinkClick}>
                           <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                             Information Security
                           </li>
                         </Link>
-                        <Link to='/service/1' onClick={handleLinkClick}>
+                        <Link to='/fit/service/1' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Consultancy Services
                         </li>
                         </Link>
-                        <Link to='/service/2' onClick={handleLinkClick}>
+                        <Link to='/fit/service/2' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Cyber Security
                         </li>
                         </Link>
-                        <Link to='/service/3' onClick={handleLinkClick}>
+                        <Link to='/fit/service/3' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Data Center Solutions
                         </li>
                         </Link>
-                        <Link to='/service/4' onClick={handleLinkClick}>
+                        <Link to='/fit/service/4' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Collaboration Suites
                         </li>
                         </Link>
-                        <Link to='/service/5' onClick={handleLinkClick}>
+                        <Link to='/fit/service/5' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Next Generation Managed Service
                         </li>
                         </Link>
-                        <Link to='/service/6' onClick={handleLinkClick}>
+                        <Link to='/fit/service/6' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Unified Communication & PABX
                         </li>
                         </Link>
-                        <Link to='/service/7' onClick={handleLinkClick}>
+                        <Link to='/fit/service/7' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Enterprise Networking
                         </li>
                         </Link>
-                        <Link to='/service/8' onClick={handleLinkClick}>
+                        <Link to='/fit/service/8' onClick={handleLinkClick}>
                         <li className="hover:bg-gray-700 px-4 py-2 rounded-md">
                           Innovation Center
                         </li>
@@ -234,7 +234,7 @@ const Navbar = () => {
                     <li>
                       <Link
                         className="block px-4 py-2 hover:bg-gray-700 rounded-lg"
-                        to="/awards"
+                        to="/fit/awards"
                       >
                         Awards
                       </Link>
@@ -242,7 +242,7 @@ const Navbar = () => {
                     <li>
                       <Link
                         className="block px-4 py-2 hover:bg-gray-700 rounded-lg"
-                        to="/contact"
+                        to="/fit/contact"
                       >
                         Contact Us
                       </Link>
